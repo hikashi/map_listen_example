39d5cdf43380418b2e7781e6c1a53b17ef798cfd
Billy Lau Pik Lik <40621513+hikashi@users.noreply.github.com>
2022-09-30 16:12:32 +0800

    Add files via upload

---
+++ map_listen/scripts/map_listen.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#--------Include modules---------------
+import rospy
+import time
+import tf
+from nav_msgs.msg import OccupancyGrid
+
+# global parameter  ------------------------------
+mapData              = OccupancyGrid()
+
+# Subscribers' callbacks ------------------------------
+def mapCallBack(data):
+	global mapData
+	mapData=data
+
+
+# main node  ------------------------------
+def node():
+    global mapData
+    rospy.init_node('map_listen', anonymous=False)
+
+    map_Topic         = rospy.get_param('~s', '/map')   # map topic
+
+
+    rospy.Subscriber(map_Topic, OccupancyGrid, mapCallBack)
+
+    # function for waiting map
+    while (len(mapData.data) < 1):
+        rospy.loginfo('Waiting for the map')
+        rospy.sleep(0.1)
+        pass
+
+    while not rospy.is_shutdown():
+        print(mapData.header)
+        print("There is map data")
+        rospy.sleep(0.5)
+
+# main function  ------------------------------
+if __name__ == '__main__':
+    try:
+        node()
+    except rospy.ROSInterruptException:
+        pass
+ 
